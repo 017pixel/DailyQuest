9695b6ebc05a7bcd8033431e809179c527e8e6a5
Benjamin Becker <beckerbenjamin2010@gmail.com>
2026-04-19 16:15:23 +0000

    fix: web worker als inline blob für github pages

--- js/timer-popup.js
+++ js/timer-popup.js
@@ -1,4 +1,61 @@
-const TIMER_WORKER_PATH = '/js/timer-worker.js';
+const TIMER_WORKER_CODE = `
+let timerInterval = null;
+let remainingTime = 0;
+let isPaused = false;
+
+self.onmessage = function(e) {
+    const { action, time } = e.data;
+    switch(action) {
+        case 'start':
+            startTimer(time);
+            break;
+        case 'pause':
+            pauseTimer();
+            break;
+        case 'resume':
+            resumeTimer();
+            break;
+        case 'stop':
+            stopTimer();
+            break;
+    }
+};
+
+function startTimer(seconds) {
+    remainingTime = seconds;
+    isPaused = false;
+    if (timerInterval) clearInterval(timerInterval);
+    timerInterval = setInterval(() => {
+        if (!isPaused && remainingTime > 0) {
+            remainingTime--;
+            self.postMessage({ action: 'tick', remaining: remainingTime });
+            if (remainingTime <= 0) {
+                clearInterval(timerInterval);
+                timerInterval = null;
+                self.postMessage({ action: 'finished' });
+            }
+        }
+    }, 1000);
+    self.postMessage({ action: 'tick', remaining: remainingTime });
+}
+
+function pauseTimer() {
+    isPaused = true;
+    self.postMessage({ action: 'paused', remaining: remainingTime });
+}
+
+function resumeTimer() {
+    isPaused = false;
+    self.postMessage({ action: 'resumed', remaining: remainingTime });
+}
+
+function stopTimer() {
+    if (timerInterval) clearInterval(timerInterval);
+    remainingTime = 0;
+    isPaused = false;
+    self.postMessage({ action: 'stopped' });
+}
+`;
 
 let timerWorker = null;
 let currentExercise = null;
@@ -23,7 +80,8 @@ window.openTimerPopup = function(exercise, questId = null) {
 };
 
 function initTimerPopup() {
-    timerWorker = new Worker(TIMER_WORKER_PATH);
+    const blob = new Blob([TIMER_WORKER_CODE], { type: 'application/javascript' });
+    timerWorker = new Worker(URL.createObjectURL(blob));
 
     timerWorker.onmessage = function(e) {
         const { action, remaining } = e.data;
